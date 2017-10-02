2640d6bc44cdde9b35441b38ac01f361debbcf35
IsNology <glenn@isnology.com>
2017-10-02 14:54:30 +1100

    add feed page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root 'landing#index'
   devise_for :users
   resources :users, only: [:show, :update], controller: :profiles
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
   resources :photos do
     resources :comments
   end
+  get '/feed' => 'feed#index'
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
